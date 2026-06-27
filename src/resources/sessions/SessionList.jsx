20f7d3c3c38240b8147d7f79a15982b52771e308
Mahefa04 <hei.mahefa.2@gmail.com>
2026-06-27 15:25:34 +0300

    feat: modification insessionList

--- src/resources/sessions/SessionList.jsx
+++ src/resources/sessions/SessionList.jsx
@@ -1,7 +1,9 @@
 import {
   List, Datagrid, TextField, DateField, ReferenceField,
   Create, Edit, SimpleForm,
-  TextInput, DateTimeInput, ReferenceInput, SelectInput, NumberInput, required
+  TextInput, DateTimeInput, ReferenceInput, SelectInput,
+  NumberInput, ReferenceArrayInput, SelectArrayInput,
+  required
 } from 'react-admin'
 
 export const SessionList = () => (
@@ -9,14 +11,14 @@ export const SessionList = () => (
     <Datagrid rowClick="edit">
       <TextField source="id" />
       <TextField source="title" label="Titre" />
-      <ReferenceField source="event_id" reference="events" label="Événement">
+      <ReferenceField source="eventId" reference="events" label="Événement">
         <TextField source="title" />
       </ReferenceField>
-      <ReferenceField source="room_id" reference="rooms" label="Salle">
+      <ReferenceField source="roomId" reference="rooms" label="Salle">
         <TextField source="name" />
       </ReferenceField>
-      <DateField source="start_time" label="Début" showTime />
-      <DateField source="end_time" label="Fin" showTime />
+      <DateField source="startTime" label="Début" showTime />
+      <DateField source="endTime" label="Fin" showTime />
     </Datagrid>
   </List>
 )
@@ -25,15 +27,18 @@ const SessionForm = () => (
   <SimpleForm>
     <TextInput source="title" label="Titre" validate={required()} fullWidth />
     <TextInput source="description" label="Description" multiline rows={4} fullWidth />
-    <ReferenceInput source="event_id" reference="events" label="Événement">
+    <ReferenceInput source="eventId" reference="events" label="Événement">
       <SelectInput optionText="title" validate={required()} />
     </ReferenceInput>
-    <ReferenceInput source="room_id" reference="rooms" label="Salle">
+    <ReferenceInput source="roomId" reference="rooms" label="Salle">
       <SelectInput optionText="name" validate={required()} />
     </ReferenceInput>
-    <DateTimeInput source="start_time" label="Heure de début" validate={required()} />
-    <DateTimeInput source="end_time" label="Heure de fin" validate={required()} />
+    <DateTimeInput source="startTime" label="Heure de début" validate={required()} />
+    <DateTimeInput source="endTime" label="Heure de fin" validate={required()} />
     <NumberInput source="capacity" label="Capacité" />
+    <ReferenceArrayInput source="speakerIds" reference="speakers" label="Intervenants">
+      <SelectArrayInput optionText="fullName" />
+    </ReferenceArrayInput>
   </SimpleForm>
 )
 
